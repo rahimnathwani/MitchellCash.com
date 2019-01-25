fbcdb5e5d6ca252b6bea56f1c4a9fffd1cb677d3
Mitchell Cash <mitchell@mitchellcash.com>
2019-01-25 17:57:59 +1000

    format

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,29 +17,29 @@ export default ({ data }) => (
       <Col sm="4"></Col>
     </Row>
     <Row>
-    <Col sm="4">
-      <h6 class="text-center">My Projects</h6>
-      <ul class="list-unstyled text-center">
-        <li><small><a class="text-muted" href="https://cryptochart.io">CryptoChart</a></small></li>
-        <li><small><a class="text-muted" href="https://github.com/mitchellcash/ownUp">ownUp</a></small></li>
-      </ul>
-    </Col>
-    <Col sm="4">
-      <h6 class="text-center">Stuff I Like</h6>
-      <ul class="list-unstyled text-center">
-        <li><small><a class="text-muted" href="https://agilebits.com/onepassword">1Password</a></small></li>
-        <li><small><a class="text-muted" href="https://www.fastmail.com">FastMail</a></small></li>
-        <li><small><a class="text-muted" href="https://mullvad.net">Mullvad</a></small></li>
-      </ul>
-    </Col>
-    <Col sm="4">
-      <h6 class="text-center">Open Source Contributions</h6>
-      <ul class="list-unstyled text-center">
-        <li><small><a class="text-muted" href="https://github.com/bitcoin/bitcoin/commits?author=mitchellcash">bitcoin/bitcoin</a></small></li>
-        <li><small><a class="text-muted" href="https://github.com/bitcoin-dot-org/bitcoin.org/commits?author=mitchellcash">bitcoin-dot-org/bitcoin.org</a></small></li>
-        <li><small><a class="text-muted" href="https://github.com/mitchellcash/">and more...</a></small></li>
-      </ul>
-    </Col>
+      <Col sm="4">
+        <h6 class="text-center">My Projects</h6>
+        <ul class="list-unstyled text-center">
+          <li><small><a class="text-muted" href="https://cryptochart.io">CryptoChart</a></small></li>
+          <li><small><a class="text-muted" href="https://github.com/mitchellcash/ownUp">ownUp</a></small></li>
+        </ul>
+      </Col>
+      <Col sm="4">
+        <h6 class="text-center">Stuff I Like</h6>
+        <ul class="list-unstyled text-center">
+          <li><small><a class="text-muted" href="https://agilebits.com/onepassword">1Password</a></small></li>
+          <li><small><a class="text-muted" href="https://www.fastmail.com">FastMail</a></small></li>
+          <li><small><a class="text-muted" href="https://mullvad.net">Mullvad</a></small></li>
+        </ul>
+      </Col>
+      <Col sm="4">
+        <h6 class="text-center">Open Source Contributions</h6>
+        <ul class="list-unstyled text-center">
+          <li><small><a class="text-muted" href="https://github.com/bitcoin/bitcoin/commits?author=mitchellcash">bitcoin/bitcoin</a></small></li>
+          <li><small><a class="text-muted" href="https://github.com/bitcoin-dot-org/bitcoin.org/commits?author=mitchellcash">bitcoin-dot-org/bitcoin.org</a></small></li>
+          <li><small><a class="text-muted" href="https://github.com/mitchellcash/">and more...</a></small></li>
+        </ul>
+      </Col>
     </Row>
   </Layout>
 )
