20b97c86800e355d6e11e44014370fae08b45824
Mitchell Cash <mitchell@mitchellcash.com>
2019-01-26 11:49:26 +1000

    More style improvements

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,7 @@ import Col from 'react-bootstrap/lib/Col'
 import Row from 'react-bootstrap/lib/Row'
 import { FaEnvelopeSquare, FaGithubSquare, FaHackerNewsSquare } from 'react-icons/fa';
 import Layout from '../layout/layout'
+import styles from './contact.module.css'
 
 export default () => (
   <Layout>
@@ -20,16 +21,16 @@ export default () => (
             <ul class="list-unstyled ml-0">
               <li>
                 <FaEnvelopeSquare className="mr-2"/>
-                <a class="first" href="mailto:mitchell@mitchellcash.com">mitchell@mitchellcash.com</a>
+                <a className={styles.contactLink} href="mailto:mitchell@mitchellcash.com">mitchell@mitchellcash.com</a>
                 <noscript>Email address protected from spambots; use a JavaScript-enabled browser to see it.</noscript>
               </li>
               <li>
                 <FaGithubSquare className="mr-2" />
-                <a href="https://github.com/mitchellcash ">GitHub</a>
+                <a className={styles.contactLink} href="https://github.com/mitchellcash ">GitHub</a>
               </li>
               <li>
                 <FaHackerNewsSquare className="mr-2" />
-                <a href="https://news.ycombinator.com/user?id=MitchellCash">Hacker News</a>
+                <a className={styles.contactLink} href="https://news.ycombinator.com/user?id=MitchellCash">Hacker News</a>
               </li>
             </ul>
           </Col>
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,14 +19,14 @@ export default ({ data }) => (
     <Row>
       <Col sm="4">
         <h6 class="text-center">My Projects</h6>
-        <ul class="list-unstyled text-center">
+        <ul class="list-unstyled text-center ml-0">
           <li><small><a class="text-muted" href="https://cryptochart.io">CryptoChart</a></small></li>
           <li><small><a class="text-muted" href="https://github.com/mitchellcash/ownUp">ownUp</a></small></li>
         </ul>
       </Col>
       <Col sm="4">
         <h6 class="text-center">Stuff I Like</h6>
-        <ul class="list-unstyled text-center">
+        <ul class="list-unstyled text-center ml-0">
           <li><small><a class="text-muted" href="https://agilebits.com/onepassword">1Password</a></small></li>
           <li><small><a class="text-muted" href="https://www.fastmail.com">FastMail</a></small></li>
           <li><small><a class="text-muted" href="https://mullvad.net">Mullvad</a></small></li>
@@ -34,7 +34,7 @@ export default ({ data }) => (
       </Col>
       <Col sm="4">
         <h6 class="text-center">Open Source Contributions</h6>
-        <ul class="list-unstyled text-center">
+        <ul class="list-unstyled text-center ml-0">
           <li><small><a class="text-muted" href="https://github.com/bitcoin/bitcoin/commits?author=mitchellcash">bitcoin/bitcoin</a></small></li>
           <li><small><a class="text-muted" href="https://github.com/bitcoin-dot-org/bitcoin.org/commits?author=mitchellcash">bitcoin-dot-org/bitcoin.org</a></small></li>
           <li><small><a class="text-muted" href="https://github.com/mitchellcash/">and more...</a></small></li>
