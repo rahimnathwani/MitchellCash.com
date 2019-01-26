48de22005650900694eabad60a78ea9c6e53f0ee
Mitchell Cash <mitchell@mitchellcash.com>
2019-01-26 17:27:52 +1000

    Web manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,49 +2,61 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: 'MitchellCash.com',
+    title: "MitchellCash.com",
     titleTemplate: "%s - MitchellCash.com",
     description: "I'm Mitchell Cash: sort of geeky, sort of not.",
-    author: 'Mitchell Cash',
-    siteUrl: 'https://mitchellcash.com',
+    author: "Mitchell Cash",
+    siteUrl: "https://mitchellcash.com",
     image: "/img/avatar.jpeg",
     twitterUsername: "@Mitch_Cash"
   },
   plugins: [
-    'gatsby-plugin-feed',
+    "gatsby-plugin-feed",
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "UA-35031246-1",
         respectDNT: true
       }
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-sitemap',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'images',
-        path: path.join(__dirname, 'src', 'assets', 'img')
+        name: "MitchellCash.com",
+        short_name: "MitchellCash",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#fff",
+        display: "standalone",
+        icon: "src/assets/img/icon.png"
+      }
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: path.join(__dirname, "src", "assets", "img")
       }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'posts',
-        path: path.join(__dirname, 'src', 'pages', 'posts')
+        name: "posts",
+        path: path.join(__dirname, "src", "pages", "posts")
       }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-reading-time',
-          'gatsby-remark-prismjs',
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-reading-time",
+          "gatsby-remark-prismjs",
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 800,
               linkImagesToOriginal: false
@@ -53,7 +65,7 @@ module.exports = {
         ]
       }
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-netlify'
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-netlify"
   ]
 }
